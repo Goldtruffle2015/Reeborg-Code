eb2c9aa87a0199ffaf9e1471258cc54a2be69b6b
Goldtruffle2015 <37117717+Goldtruffle2015@users.noreply.github.com>
2018-03-10 11:00:21 -0700

    Update New and Improved Maze Solver with Flowers.py

--- New and Improved Maze Solver with Flowers.py
+++ New and Improved Maze Solver with Flowers.py
@@ -3,7 +3,7 @@
 "Author: John Yu
 "Date: March 9, 2018"
 '''
-think(0)
+think(50)
 set_max_nb_steps(10000)
 # variables
 def turn_right():
@@ -12,59 +12,52 @@ def turn_right():
 def turn_around():
     repeat 2:
         turn_left()
-Reeborg_Turned_Right = False 
+Reeborg_Turned_Right = False
+finishline = 0
 # CODE STARTS HERE
-if at_goal():
-    turn_around()
-    move()
-    while not at_goal():
-        while object_here():
-            take()
-        if right_is_clear():
-            turn_right()
-            move()
-        elif front_is_clear():
-            move()
+while finishline == 0 or finishline == 1:
+    if at_goal():
+        turn_around()
+        move()
+        finishline += 1
+    else:
+        pass
+    while object_here():
+        take()
+    walls = 0
+    repeat 4:
+        if wall_in_front():
+            walls += 1
+            turn_left()
         else:
             turn_left()
-else:
-    while not at_goal():
-        while object_here():
-            take()
-        walls = 0
-        repeat 4:
-            if wall_in_front():
-                walls += 1
+    if walls == 4:
+        done()
+    else:
+        if walls == 3:
+            while not front_is_clear():
                 turn_left()
-            else:
-                turn_left()
-        if walls == 4:
-            done()
+            move()
+            turn_around()
+            build_wall()
+            turn_around()
         else:
-            if walls == 3:
-                while not front_is_clear():
-                    turn_left()
-                move()
-                turn_around()
-                build_wall()
-                turn_around()
+            if Reeborg_Turned_Right == False:
+                rightNum = 0
             else:
-                if Reeborg_Turned_Right == False:
-                    rightNum = 0
+                pass
+            if right_is_clear():
+                turn_right()
+                rightNum += 1
+                Reeborg_Turned_Right = True
+                move()
+                if rightNum > 4:
+                    turn_left()
                 else:
                     pass
-                if right_is_clear():
-                    turn_right()
-                    rightNum += 1
-                    Reeborg_Turned_Right = True
+            else:
+                Reeborg_Turned_Right = False
+                if front_is_clear():
                     move()
-                    if rightNum > 4:
-                        turn_left()
-                    else:
-                        pass
                 else:
-                    Reeborg_Turned_Right = False
-                    if front_is_clear():
-                        move()
-                    else:
-                        turn_left()
+                    turn_left()
