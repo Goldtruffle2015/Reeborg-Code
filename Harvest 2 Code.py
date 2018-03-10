7bb8d34c2f4535ef4ff3650106453e3b2e09b9c8
Goldtruffle2015 <37117717+Goldtruffle2015@users.noreply.github.com>
2018-03-09 17:17:55 -0700

    Add files via upload

---
+++ Harvest 2 Code.py
@@ -0,0 +1,37 @@
+think(0)
+set_max_nb_steps(10000)
+'''
+"Title: Harvest 2 Code"
+"Author: John Yu"
+"Date: March 9, 2018"
+'''
+# Variables
+def turn_right():
+    repeat 3:
+        turn_left()
+carrotNum = 0
+# Code Starts Here
+while carrotNum < 1 or not carrots_in_column == 0:
+    ### --- Get to standard orientation --- ###
+    if is_facing_north():
+        if front_is_clear():
+            ### --- Reset endNum --- ###
+            carrots_in_column = 0
+            ### --- Search Column 1 --- ###
+            while front_is_clear():
+                if object_here():
+                    take()
+                    carrotNum += 1
+                    carrots_in_column += 1
+                else:
+                    move()
+        ### --- Turn around and go back down --- ###
+        else:
+            turn_right()
+            move()
+            turn_right()
+            while front_is_clear():
+                move()
+    ### --- turn until facing north --- ###
+    else:
+        turn_left()
