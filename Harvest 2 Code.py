87482f9a76f58551459bb518a4e309fd39ccdf8e
Goldtruffle2015 <37117717+Goldtruffle2015@users.noreply.github.com>
2018-03-12 13:46:46 -0600

    Update Harvest 2 Code.py

--- Harvest 2 Code.py
+++ Harvest 2 Code.py
@@ -5,19 +5,20 @@ set_max_nb_steps(10000)
 "Author: John Yu"
 "Date: March 9, 2018"
 '''
-# Variables
+# --- Subroutines ---#
 def turn_right():
     repeat 3:
         turn_left()
+# --- Variables ---#
 carrotNum = 0
-# Code Starts Here
+# --- Code Starts Here ---#
 while carrotNum < 1 or not carrots_in_column == 0:
-    ### --- Get to standard orientation --- ###
+    #Get to standard orientation
     if is_facing_north():
         if front_is_clear():
-            ### --- Reset endNum --- ###
+            #Reset carrots_in_column
             carrots_in_column = 0
-            ### --- Search Column 1 --- ###
+            #Search Columns
             while front_is_clear():
                 if object_here():
                     take()
@@ -25,13 +26,14 @@ while carrotNum < 1 or not carrots_in_column == 0:
                     carrots_in_column += 1
                 else:
                     move()
-        ### --- Turn around and go back down --- ###
+        #Turn around and go back down
         else:
             turn_right()
             move()
             turn_right()
             while front_is_clear():
                 move()
-    ### --- turn until facing north --- ###
+    #turn until facing north
     else:
         turn_left()
+done()
